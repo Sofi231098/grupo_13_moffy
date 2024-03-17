63f9b4dda9f880f98f67bb09234c03a0eba41267
MaxiCarrillo <maxi11carrill@gmail.com>
2024-03-16 21:16:11 -0300

    Update header

--- Aplicacion/src/controllers/mainController.js
+++ Aplicacion/src/controllers/mainController.js
@@ -1,11 +1,3 @@
-const { validationResult } = require('express-validator');
-const usuarios = require('../data/usersDataBase.json');
-const crypto = require('crypto');
-const fs = require('fs');
-const path = require('path');
-const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
-const bcrypt = require('bcryptjs');
-
 const mainController = {
     index: (req, res) => {
         return res.render('home');
--- Aplicacion/src/middlewares/UserLoggedMiddleware.js
+++ Aplicacion/src/middlewares/UserLoggedMiddleware.js
@@ -1,13 +1,26 @@
 const users = require('../data/usersDataBase.json');
 
+/**
+ * Middleware que verifica si el usuario está logueado y guarda la información en res.locals.
+ * @param {Object} req - El objeto de solicitud HTTP.
+ * @param {Object} res - El objeto de respuesta HTTP.
+ * @param {Function} next - La función de siguiente middleware.
+ */
 const UserLoggedMiddleware = (req, res, next) => {
     res.locals.isLogged = false; 
-    let userEmail = req.cookies.userEmail;
-    let user = users.find(user => user.email === userEmail);
-    if(user){
-        req.session.user = user;
+    // Verificar si el usuario tiene una cookie con su email
+    if(req.cookies.userEmail){
+        // Buscar al usuario que corresponde a ese email
+        let userEmail = req.cookies.userEmail;
+        let user = users.find(user => user.email === userEmail);
+        // Si el usuario existe, lo guardamos en session
+        if(user){
+            req.session.user = user;
+        }
     }
+    // Verificar si el usuario está logueado
     if(req.session.user){
+        // Si está logueado, lo guardamos en res.locals para que esté disponible en todas las vistas
         res.locals.isLogged = true; 
         res.locals.user = req.session.user;
     }
